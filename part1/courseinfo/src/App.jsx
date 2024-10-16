050a61e4517bc5304c3a7f957931cbb20bf11c7e
Santiago Tena Hernandez <stenahe@gmail.com>
2024-10-16 22:08:20 +0200

    add solution to step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,9 +14,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content  parts={[part1, part2, part3]} 
+                exercises={[exercises1, exercises2, exercises3]} />
       <Total  total_exercises={exercises1 + exercises2 +exercises3} />
     </div>
   )
--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,10 +1,13 @@
+import Part from './Part'
+
 const Content = (props) => {
 	return (
-		<p>
-			{props.part} {props.exercises}
-		</p>
+		<>
+			<Part part={props.parts[0]} exercises={props.exercises[0]} />
+			<Part part={props.parts[1]} exercises={props.exercises[1]} />
+			<Part part={props.parts[2]} exercises={props.exercises[2]} />
+		</>
 	)
-
 }
 
 export default Content
