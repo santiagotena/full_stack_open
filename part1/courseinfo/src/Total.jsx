625268ee16a5e9c35298d637f9217886b1202e0f
Santiago Tena Hernandez <stenahe@gmail.com>
2024-10-17 14:08:03 +0200

    add step 3 solution

--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,7 +1,9 @@
 const Total = (props) => {
 	return (
 		<p>
-			Number of exercises {props.total_exercises}
+			Number of exercises {props.parts[0].exercises +
+													props.parts[1].exercises+
+													props.parts[2].exercises}
 		</p>
 	)
 }
