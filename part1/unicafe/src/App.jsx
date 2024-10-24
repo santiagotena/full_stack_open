9a28f914bfa45a9f6c6ff55174210ddd01be2ade
Santiago Tena Hernandez <stenahe@gmail.com>
2024-10-24 15:13:59 +0200

    refactor

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,15 +13,6 @@ const Button = ({handleClick, text}) => {
 }
 
 const StatisticLine = ({text, value}) => {
-  if (text === 'positive') {
-    return(
-      <tr>
-        <td>{text}</td>
-        <td>{value} %</td>
-      </tr>
-    )
-  }
-
   return(
     <tr>
       <td>{text}</td>
@@ -49,7 +40,7 @@ const Statistics = ({good, neutral, bad}) => {
         <StatisticLine text='bad' value={bad}/>
         <StatisticLine text='all' value={all}/>
         <StatisticLine text='average' value={average}/>
-        <StatisticLine text='positive' value={positive}/>
+        <StatisticLine text='positive' value={positive + ' %'}/>
       </tbody>
     </table>
   )
