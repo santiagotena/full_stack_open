1404ad16d20c7b98d5271b66311576e2c49f65b3
Santiago Tena Hernandez <stenahe@gmail.com>
2024-10-31 14:08:28 +0100

    solve until 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const InputField = ({ label, inputValue, onInputChange }) => {
   return(
@@ -55,15 +56,18 @@ const Contacts = ({ contacts, filterName }) => {
 }
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [contacts, setContacts] = useState([]) 
   const [filterName, setFilterName] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setContacts(response.data)
+      })
+  }, [])
 
   const addContact = (event) => {
     event.preventDefault()
