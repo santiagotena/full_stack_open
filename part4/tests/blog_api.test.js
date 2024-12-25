eb481c0cd30906e12bc00d5cbeaa3c568d573843
Santiago Tena Hernandez <stenahe@gmail.com>
2024-12-25 23:53:37 +0100

    setup exercises

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,24 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+
+
+after(async () => {
+  await mongoose.connection.close()
+})
---
+++ part4/tests/test_helper.js
@@ -0,0 +1,40 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "Test",
+		author: "Max",
+		url: "http://abc",
+		likes: 4
+  },
+  {
+    title: "Test2",
+		author: "Mustermann",
+		url: "http://cba",
+		likes: 2
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog(
+		{
+			title: "abc",
+			author: "abc",
+			url: "http://aaa",
+			likes: 0
+		}
+	)
+  await blog.save()
+  await blog.deleteOne()
+
+  return blog._id.toString()
+}
+
+const notesInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, notesInDb
+}
