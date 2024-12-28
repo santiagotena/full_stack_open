9a872cdc163175a90a63a258e16b19659c018360
Santiago Tena Hernandez <stenahe@gmail.com>
2024-12-28 22:04:00 +0100

    solve until 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -22,11 +22,31 @@ test('there are two blogs', async() => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
-test.only('blogs should have a unique identifier property named "id"', async () => {
+test('blogs should have a unique identifier property named "id"', async () => {
   const response = await api.get('/api/blogs')
   response.body.forEach((item) => {
     assert.ok('id' in item)
   })
+})
+
+test.only('a blog entry can be added', async() => {
+  const newBlog = {
+    title: 'test title',
+    author: 'John Doe',
+    url: 'url.com',
+    likes: 5,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  const notesAtEnd = await helper.notesInDb()
+  assert.strictEqual(notesAtEnd.length, helper.initialBlogs.length + 1)
+
+  const titles = notesAtEnd.map(n => n.title)
+  assert(titles.includes('test title'))
 })
 
 after(async () => {
