4258f65a5609a9fc71e7ddfc9c2f34d65c213117
Santiago Tena Hernandez <stenahe@gmail.com>
2025-01-06 00:28:55 +0100

    refactor

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -11,6 +11,75 @@ const helper = require('./test_helper')
 const User = require('../models/user')
 const Blog = require('../models/blog')
 
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('user creation fails if username or password are missing', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const noUsername = {
+      name: 'Name',
+      password: 'password',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(noUsername)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    assert(result.body.error.includes('username and password are required'))
+
+    const noPassword = {
+      username: 'username',
+      name: 'Name',
+    }
+
+    const result2 = await api
+      .post('/api/users')
+      .send(noPassword)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd2 = await helper.usersInDb()
+
+    assert.strictEqual(usersAtEnd2.length, usersAtStart.length)
+    assert(result2.body.error.includes('username and password are required'))
+  })
+})
+
 describe('when there is initially some notes saved', () => {
 
   beforeEach(async () => {
@@ -133,75 +202,6 @@ describe('when there is initially some notes saved', () => {
     assert(updatedBlogInDb)
     assert.strictEqual(updatedBlogInDb.title, updatedBlog.title)
     assert.strictEqual(updatedBlogInDb.likes, updatedBlog.likes)
-  })
-})
-
-describe('when there is initially one user at db', () => {
-  beforeEach(async () => {
-    await User.deleteMany({})
-
-    const passwordHash = await bcrypt.hash('sekret', 10)
-    const user = new User({ username: 'root', passwordHash })
-
-    await user.save()
-  })
-
-  test('creation succeeds with a fresh username', async () => {
-    const usersAtStart = await helper.usersInDb()
-
-    const newUser = {
-      username: 'mluukkai',
-      name: 'Matti Luukkainen',
-      password: 'salainen',
-    }
-
-    await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const usersAtEnd = await helper.usersInDb()
-    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
-
-    const usernames = usersAtEnd.map(u => u.username)
-    assert(usernames.includes(newUser.username))
-  })
-
-  test('user creation fails if username or password are missing', async () => {
-    const usersAtStart = await helper.usersInDb()
-
-    const noUsername = {
-      name: 'Name',
-      password: 'password',
-    }
-
-    const result = await api
-      .post('/api/users')
-      .send(noUsername)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-
-    const usersAtEnd = await helper.usersInDb()
-
-    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
-    assert(result.body.error.includes('username and password are required'))
-
-    const noPassword = {
-      username: 'username',
-      name: 'Name',
-    }
-
-    const result2 = await api
-      .post('/api/users')
-      .send(noPassword)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-
-    const usersAtEnd2 = await helper.usersInDb()
-
-    assert.strictEqual(usersAtEnd2.length, usersAtStart.length)
-    assert(result2.body.error.includes('username and password are required'))
   })
 })
 
